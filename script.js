2287fd44f94372f262f9a3e674b7890887829e47
Deresan <gabrieldavinci69@gmail.com>
2023-02-22 20:28:59 +0200

    create playerSelection function

--- script.js
+++ script.js
@@ -3,7 +3,6 @@
 function getComputerChoice()
 {
     let num = Math.ceil(Math.random()*3);
-    console.log(num);
     let compChoice = "";
 
     switch(num){
@@ -24,4 +23,19 @@ function getComputerChoice()
     return compChoice;
 }
 
+const getPlayerSelection = () => prompt("Rock, Paper , Scissors");
+
+    //console.log(a.localeCompare(b,'en',{sensitivity: 'base'})); compares 2 strings ignoring case
+
+
+function playRound(playerSelection, computerChoice){
+    
+
+}
+
+
 console.log(getComputerChoice());
+const player = getPlayerSelection();
+const comp = getComputerChoice();
+
+playRound(player, comp);
