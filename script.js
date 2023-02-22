f2a33a8b2aa2364faa4e38e7efb4639389a5dcc9
Deresan <gabrieldavinci69@gmail.com>
2023-02-22 21:56:36 +0200

    complete game function, add game history and result

--- script.js
+++ script.js
@@ -20,12 +20,14 @@ function getComputerChoice()
             break;
     }
 
-    console.log(compChoice);
+    //console.log(compChoice);
 
     return compChoice;
 }
 
-const getPlayerSelection = () => prompt("Rock, Paper , Scissors");
+function getPlayerSelection(){
+    return prompt("Rock, Paper , Scissors");
+} 
 
     //console.log(a.localeCompare(b,'en',{sensitivity: 'base'})); compares 2 strings ignoring case
 
@@ -44,13 +46,13 @@ function playRound(playerSelection, computerChoice){
             playerSelection == 'paper' && computerChoice == 'rock' ||
             playerSelection == 'scissors' && computerChoice == 'paper'){
 
-                result = 'You Win! ' +playerSelection + ' beats '+ computerChoice;    
+                result = 'You Win!'; //+playerSelection + ' beats '+ computerChoice;    
     }
     else if(playerSelection == 'scissors' && computerChoice == 'rock' || 
             playerSelection == 'rock' && computerChoice == 'paper' ||
             playerSelection == 'paper' && computerChoice == 'scissors'){
 
-            result = 'You Lose! ' +computerChoice + ' beats '+playerSelection;
+            result = 'You Lose!'; //+computerChoice + ' beats '+playerSelection;
     }
     else{
         result = 'Wrong input! You typed: '+playerSelection;
@@ -60,8 +62,59 @@ function playRound(playerSelection, computerChoice){
 
 }
 
-const comp = getComputerChoice();
-const player = getPlayerSelection();
+function game(){
+    let arrResults = [];
+    let arrHistory = [];
+    let winner ='';
+
+    for (let i = 0; i < 5; i++) {
+        let comp = getComputerChoice();
+        let player = getPlayerSelection();
+        arrResults.push(playRound(player,comp));
+        arrHistory.push("Player Choice: "+ player+", Computer choice: "+comp+", Result: "+ playRound(player,comp));
+    }
+
+    let draws = 0;
+    let wins = 0;
+    let loses = 0;
+
+    for(i =0; i< arrResults.length;i++){
+        if(arrResults[i].toLowerCase() == 'draw!')
+        {
+            draws++;
+        }
+        else if(arrResults[i].toLowerCase() == 'you win!'){
+            wins++;
+        }
+        else if(arrResults[i].toLowerCase() == 'you lose!'){
+            loses++;
+        }
+    }
+
+    if(wins > loses && wins > draws){
+        winner = "Player is the Winner";
+    }
+    else if(loses > wins && loses > draws){
+        winner = "Computer is the Winner";
+    }
+    else if(draws > loses && draws > loses || wins == loses){
+        winner = "Player and Computer Tie";
+    }
+
+    for(i =0; i<arrHistory.length;i++){
+
+        console.log(arrHistory[i]+'\n');
+    }
+
+    console.log("Draws: "+draws +"\nWins:  "+ wins+ "\nLoses: "+loses);
+    console.log(winner);
+    
+}
+
+//const comp = getComputerChoice();
+//const player = getPlayerSelection();
 
 
-console.log(playRound(player, comp));
+//console.log(playRound(player, comp));
+
+game();
