3320b13ca63aa9e57fc4d44684dd24b14a2a3f11
Deresan <gabrieldavinci69@gmail.com>
2023-02-22 19:40:58 +0200

    create getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("Hello");
+//function to randomly get computer choice
+
+function getComputerChoice()
+{
+    let num = Math.ceil(Math.random()*3);
+    console.log(num);
+    let compChoice = "";
+
+    switch(num){
+
+        case 1:
+            compChoice = "Rock";
+            break;
+
+        case 2: 
+            compChoice = "Paper";
+            break;
+
+        case 3:
+            compChoice = "Scissors"
+            break;
+    }
+
+    return compChoice;
+}
+
+console.log(getComputerChoice());
