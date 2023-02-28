1d9ba54807adc60a2de5d91abf0e5f4a9f8176f0
Deresan <gabrieldavinci69@gmail.com>
2023-02-28 16:05:34 +0200

    add buttons and score calc

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 //function to randomly get computer choice
 
+ let playerScore = 0;
+ let computerScore = 0;
+
+
 function getComputerChoice()
 {
     let num = Math.ceil(Math.random()*3);
@@ -46,11 +50,15 @@ function playRound(playerSelection, computerChoice){
             playerSelection == 'paper' && computerChoice == 'rock' ||
             playerSelection == 'scissors' && computerChoice == 'paper'){
 
-                result = 'You Win!'; //+playerSelection + ' beats '+ computerChoice;    
+            playerScore++;    
+
+            result = 'You Win!'; //+playerSelection + ' beats '+ computerChoice;    
     }
     else if(playerSelection == 'scissors' && computerChoice == 'rock' || 
             playerSelection == 'rock' && computerChoice == 'paper' ||
             playerSelection == 'paper' && computerChoice == 'scissors'){
+
+            computerScore++;    
 
             result = 'You Lose!'; //+computerChoice + ' beats '+playerSelection;
     }
@@ -58,11 +66,34 @@ function playRound(playerSelection, computerChoice){
         result = 'Wrong input! You typed: '+playerSelection;
     }
 
-    return result;
+    console.log('You played '+ playerSelection + ' computer played '+computerChoice+' '+result);
+
+    const resultsContainer = document.querySelector('#results');
+
+    resultsContainer.innerText = 'You played '+ playerSelection + ' computer played '+computerChoice+' '+result;
+    
+    const pScore = document.querySelector('#player');
+    const cScore = document.querySelector('#computer');
+
+    pScore.innerText = `Player :${playerScore}`;
+    cScore.innerText = `Computer:${computerScore}`;
+
+    if(playerScore == 5 || computerScore == 5){
+        if(playerScore > computerScore){
+            resultsContainer.innerText='You played '+ playerSelection + ' computer played '+computerChoice+' '+"You are the first to 5 wins";
+        }
+        else{
+            resultsContainer.innerText='You played '+ playerSelection + ' computer played '+computerChoice+' '+"Computer is the first to 5 wins";
+        }
+
+        playerScore = 0;
+        computerScore = 0;
+
+    }
 
 }
 
-function game(){
+/*function game(){
     let arrResults = [];
     let arrHistory = [];
     let winner ='';
@@ -109,7 +140,21 @@ function game(){
     console.log("Draws: "+draws +"\nWins:  "+ wins+ "\nLoses: "+loses);
     console.log(winner);
     
-}
+}*/
+
+const btnRock = document.querySelector('#btn-rock');
+
+btnRock.addEventListener('click', ()=>playRound('rock',getComputerChoice()));
+
+const btnPaper = document.querySelector('#btn-paper');
+
+btnPaper.addEventListener('click', ()=>playRound('paper',getComputerChoice()));
+
+const btnScissors = document.querySelector('#btn-scissors');
+
+btnScissors.addEventListener('click', ()=>playRound('scissors',getComputerChoice()));
+
+
 
 //const comp = getComputerChoice();
 //const player = getPlayerSelection();
@@ -117,4 +162,3 @@ function game(){
 
 //console.log(playRound(player, comp));
 
-game();
